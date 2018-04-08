649d002cad355fd0a9b4076d4b9e70f1cc468b3b
minghuatang <shuyuetuo@gmail.com>
2018-04-08 17:15:22 +0800

    feat: finish stage3

--- src/main.cc
+++ src/main.cc
@@ -2,7 +2,7 @@
 
 int main(int argc, char* argv[]) {
   WC wc(argv[1]);
-  auto ws = wc.simple_count();
+  auto ws = wc.parallel_count();
   output_result(ws);
 
   return 0;
--- src/word_count.cc
+++ src/word_count.cc
@@ -10,9 +10,6 @@
 using namespace std;
 
 WC::WordSet WC::simple_count() {
-  if (!_fp) {
-    return {};
-  }
   int size = file_length();
   char* buff = new (nothrow) char[size + 1];
   if (!buff) {
@@ -23,6 +20,40 @@ WC::WordSet WC::simple_count() {
   WordMap wm = word_count(string(buff));
 
   return word_sort(wm);
+}
+
+WC::WordSet WC::parallel_count() {
+  const int block_length = 81920;
+  int size = file_length();
+
+  vector<future<WordMap>> future_results;
+  for (int i = 0; i < size / block_length + 1; i++) {
+    char* buff = new (nothrow) char[block_length + 1024];
+    if (!buff) {
+      return {};
+    }
+
+    if (fread(buff, 1, block_length, _fp) == block_length) {
+      char* bp = buff + block_length;
+      for (char c = fgetc(_fp); c && !isspace(c); c = fgetc(_fp), bp++) {
+        *bp = c;
+      }
+      *bp = '\0';
+    }
+
+    future_results.push_back(_pool.enqueue(word_count, string(buff)));
+    delete[] buff;
+  }
+  // merge all results
+  WordMap result;
+  for (auto& fr : future_results) {
+    WordMap smap = std::move(fr.get());
+    for (auto& p : smap) {
+      result[p.first] += p.second;
+    }
+  }
+
+  return word_sort(result);
 }
 
 WC::WordMap word_count(string buff) {
